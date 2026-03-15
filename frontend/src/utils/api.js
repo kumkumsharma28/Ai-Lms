a7518e3a19b25f127dc2d34db2a14746a7ab9899
kumkumsharma28 <kumkumsharma2802@gmail.com>
2026-03-15 22:01:15 +0530

    Fix API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,7 @@
-// src/utils/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL + '/api' : '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
